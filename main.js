c80ed9e41826906360fdd2bb5f68eaf750a4fd2e
timtim <timtim@mail.ru>
2026-01-22 09:04:45 +0300

    Homework-4

---
+++ main.js
@@ -0,0 +1,48 @@
+// Покраска всех карточек
+
+const productCard = document.querySelectorAll('.card-container');
+const changeColorCardButton = document.querySelector('#change-color-all-card');
+const greenColorHash = '#28300d';
+const blueColor = '#5757da';
+
+changeColorCardButton.addEventListener('click', () => {
+  productCard.forEach((card) => card.style.backgroundColor = greenColorHash);
+})
+
+// Покраска первой карточки
+
+const firstProductCard = document.querySelector('.card-container');
+const changeColorFirstCardButton = document.querySelector('#change-color-first-card');
+
+changeColorFirstCardButton.addEventListener('click', () => {
+  firstProductCard.style.backgroundColor = blueColor;
+})
+
+// Открытие гугл карты
+
+const openGoogleButton = document.querySelector('#open-google');
+
+openGoogleButton.addEventListener('click', openGoogle)
+
+function openGoogle() {
+  const isConfirmed = confirm('Вы действительно хотите открыть Google?');
+
+  if (isConfirmed === true) {
+    window.open('https://www.google.com');
+   } else {
+      return;
+    }  
+}
+
+//Вывод консоль лог
+
+const outConsoleLogButton = document.querySelector('#output-console-log');
+
+//outConsoleLogButton.addEventListener('click', outconsolelog)
+
+outConsoleLogButton.addEventListener('click', outconsolelog('ДЗ №4'))
+ 
+function outconsolelog(message)  {
+  alert(message);
+  console.log(message);
+}
